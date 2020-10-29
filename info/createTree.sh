611ff729ba30d362ea26c8dafe1d1081ecc4399b
unknown <rcarnevale89@hotmail.com>
2020-10-28 22:16:22 -0300

    home component criado

--- info/createTree.sh
+++ info/createTree.sh
@@ -5,6 +5,16 @@
     # mkdir ./src/app/shared
     # mkdir ./src/app/design-system
 
+# home
+    # mkdir -p ./src/app/modules/home/components
+    # mkdir -p ./src/app/modules/home/pages    
+    # mkdir -p ./src/app/modules/home/services/DTO/request
+    # mkdir -p ./src/app/modules/home/services/DTO/response
+    # npx ng g m modules/home/
+    # npx ng g s modules/home/services/home --skipTests=true
+    # npx ng g c modules/home/pages/index
+    # npx ng generate module modules/home/home-routing --flat --module=home
+
 
 # party-resignation-date
     # mkdir -p ./src/app/modules/party-resignation-date/components
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IndexComponent } from './pages/index/index.component';
+import { HomeRoutingModule } from './home-routing.module';
+
+
+
+@NgModule({
+  declarations: [IndexComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
