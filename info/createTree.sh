9d4edad576f0b56abf45d6d0b94b15fec27ff3b0
unknown <rcarnevale89@hotmail.com>
2020-10-28 21:57:38 -0300

    commit inicial

---
+++ info/createTree.sh
@@ -0,0 +1,17 @@
+# Create estrutura
+
+# Create paths
+    # mkdir ./src/app/modules
+    # mkdir ./src/app/shared
+    # mkdir ./src/app/design-system
+
+
+# party-resignation-date
+    # mkdir -p ./src/app/modules/party-resignation-date/components
+    # mkdir -p ./src/app/modules/party-resignation-date/pages    
+    # mkdir -p ./src/app/modules/party-resignation-date/services/DTO/request
+    # mkdir -p ./src/app/modules/party-resignation-date/services/DTO/response
+    # npx ng g m modules/party-resignation-date/
+    # npx ng g s modules/party-resignation-date/services/party-resignation-date --skipTests=true
+    # npx ng g c modules/party-resignation-date/pages/index  --spec=false
+    # npx ng generate module modules/party-resignation-date/party-resignation-date-routing --flat --module=party-resignation-date
---
+++ src/app/modules/party-resignation-date/party-resignation-date.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PartyResignationDateRoutingModule } from './party-resignation-date-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PartyResignationDateRoutingModule
+  ]
+})
+export class PartyResignationDateModule { }
