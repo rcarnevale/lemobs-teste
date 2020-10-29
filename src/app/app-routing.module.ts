07a9debeb352457720159d0486bf25994f4e4e89
unknown <rcarnevale89@hotmail.com>
2020-10-28 22:30:02 -0300

    criando rotas apontando pros modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+  path: '',
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./modules/home/home.module').then(m => m.HomeModule)
+      }
+    ]
+  },
+  {
+    path: 'party-resignation-category',
+      children: [
+        {
+          path: '',
+          loadChildren: () =>
+            import('./modules/party-resignation-category/party-resignation-category.module').then(m => m.PartyResignationCategoryModule)
+        }
+      ]
+    },
+    {
+      path: 'party-resignation-date',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./modules/party-resignation-date/party-resignation-date.module').then(m => m.PartyResignationDateModule)
+          }
+        ]
+      },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
